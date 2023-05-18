1a9e0deab8a7e1df4a504f71832dd37a2a26668a
Victor Santele <victor.santele@gmail.com>
2023-05-19 01:47:33 +0200

    Update default port

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER") or "mysqlUser"
 DB_PASSWORD = os.getenv("DB_PASSWORD") or "mysqlPassword"
 DB_HOST = os.getenv("DB_HOST") or "localhost"
-DB_PORT = int(os.getenv("DB_PORT") or "3306")
+DB_PORT = int(os.getenv("DB_PORT") or "3300")
 DB_NAME = os.getenv("DB_NAME") or "CLICOM"
