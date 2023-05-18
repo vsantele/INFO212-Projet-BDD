34f8356d94520e289dfa841410c9604981149ce4
Victor Santele <victor.santele@gmail.com>
2023-05-19 01:09:37 +0200

    Add default config value

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT"))
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER") or "mysqlUser"
+DB_PASSWORD = os.getenv("DB_PASSWORD") or "mysqlPassword"
+DB_HOST = os.getenv("DB_HOST") or "localhost"
+DB_PORT = int(os.getenv("DB_PORT") or "3306")
+DB_NAME = os.getenv("DB_NAME") or "CLICOM"
